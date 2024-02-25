b2414b898da419fb35b1fa439b7be30310d7807b
doploz <garzonjulian928@gmail.com>
2024-02-25 16:36:27 -0500

    agregar el middleware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,9 +16,29 @@ class Controller extends BaseController
 
     public function getAllUsers()
     {
-        $users = DB::table('users')->get();
-
-        return ['users' => $users];
+        // Inicializar un arreglo para almacenar los últimos registros de cada máquina
+        $lastRecordsByMachine = [];
+    
+        // Obtener los números de máquina distintos presentes en la tabla
+        $machines = [1, 2, 3, 4]; // Definir el orden deseado de las máquinas
+    
+        // Iterar sobre cada número de máquina en el orden específico deseado
+        foreach ($machines as $machine) {
+            // Consultar los últimos 10 registros de la máquina actual
+            $lastRecords = DB::table('Metadata')
+                ->where('MAQUINA', $machine)
+                ->orderBy('inserDT', 'desc')
+                ->limit(10)
+                ->get();
+    
+            // Almacenar los registros en el arreglo $lastRecordsByMachine
+            $lastRecordsByMachine["MAQUINA_$machine"] = $lastRecords;
+        }
+    
+        // Devolver el arreglo con los últimos registros de cada máquina
+        return response()->json($lastRecordsByMachine);
     }
+    
 
+//ttp://localhost:8000/getAlldata
 }
